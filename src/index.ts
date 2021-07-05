56204388095e2f2612fa804e6b917f1e62047c2f
Eric Puskas <eric.puskas@yahoo.com>
2021-07-05 21:06:28 +0300

    0.1.9

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export * from "./components/index";
+/**
+ * Exports components
+ */
+export * from "./TestComponent";
+export * from "./Button";
+
+/**
+ * Exports hooks
+ */
 export * from "./hooks/index";
